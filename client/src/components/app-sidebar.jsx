a9e5b5c827f777931cf51cf96c21a3d5a09c9721
unknown <vishisht22868@gmail.com>
2025-06-02 23:40:52 +0530

    Set up shadcn and its pretty cool.. added a sideboard component from the library

---
+++ client/src/components/app-sidebar.jsx
@@ -0,0 +1,144 @@
+import * as React from "react"
+import { GalleryVerticalEnd, Minus, Plus } from "lucide-react"
+
+import { SearchForm } from "@/components/search-form"
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+const data = {
+  navMain: [
+    {
+      title: "Teachers",
+      url: "#",
+      items: [
+        {
+          title: "Add",
+          url: "#"
+        },
+        {
+          title: "View All",
+          url: "#"
+        },
+        {
+          title: "View Schedule",
+          url: "#",
+        },
+        {
+          title: "Update Schedule",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Classrooms",
+      url: "#",
+      items: [
+        {
+          title: "Add",
+          url: "#",
+        },
+        {
+          title: "View All",
+          url: "#"
+        },
+        {
+          title: "View Schedule",
+          url: "#",
+        },
+        {
+          title: "Update Schedule",
+          url: "#"
+        }
+      ],
+    },
+    {
+      title: "Subjects",
+      url: "#",
+      items: [
+        {
+          title: "Add",
+          url: "#",
+        },
+        {
+          title: "View Associated Teachers",
+          url: "#",
+        },
+      ],
+    }
+  ],
+}
+
+export function AppSidebar({
+  ...props
+}) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div
+                  className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                  <GalleryVerticalEnd className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-medium">IIIT Bhopal Scheduler</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            {data.navMain.map((item, index) => (
+              <Collapsible key={item.title} defaultOpen={index === 1} className="group/collapsible">
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton>
+                      {item.title}{" "}
+                      <Plus className="ml-auto group-data-[state=open]/collapsible:hidden" />
+                      <Minus className="ml-auto group-data-[state=closed]/collapsible:hidden" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  {item.items?.length ? (
+                    <CollapsibleContent>
+                      <SidebarMenuSub>
+                        {item.items.map((item) => (
+                          <SidebarMenuSubItem key={item.title}>
+                            <SidebarMenuSubButton asChild isActive={item.isActive}>
+                              <a href={item.url}>{item.title}</a>
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                        ))}
+                      </SidebarMenuSub>
+                    </CollapsibleContent>
+                  ) : null}
+                </SidebarMenuItem>
+              </Collapsible>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
