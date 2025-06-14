0d3f5b23f37a4cf6a0c36a8cbc4be3d8bc59ae30
unknown <vishisht22868@gmail.com>
2025-06-15 03:21:09 +0530

    simplifying contructor structuring to not include or handle links within the contructors, all links will be handled during slot additions and updates

--- server/constructors/subjectConstructor.js
+++ server/constructors/subjectConstructor.js
@@ -16,13 +16,10 @@ export default class Subject {
      * @param {string} name - The name of the subject. Will be capitalized.
      * @param {string} code - The unique code for the subject. Will be normalized.
      * @param {string} type - The type of the subject, either "theory" or "lab". Will be normalized.
-     * @param {string[]} [teachers=[]] - An optional array of teacher codes. Will be normalized.
-     *
      * @throws {Error} If name, code, or type is missing.
      * @throws {Error} If the provided type is not one of the valid types ("theory", "lab").
-     * @throws {Error} If teachers is not an array.
      */
-    constructor(name, code, type, teachers = []) {
+    constructor(name, code, type) {
         if (!code || !name || !type) {
             throw new Error("Subject must have a code, name and type");
         }
@@ -49,15 +46,5 @@ export default class Subject {
          * @description Normalized type of the subject. Either "theory" or "lab".
          */
         this.type = normalizedType;
-
-        if (!Array.isArray(teachers)) {
-            throw new Error("Teachers must be an array");
-        }
-
-        /**
-         * @type {string[]}
-         * @description Normalized array of teacher codes associated with the subject.
-         */
-        this.teachers = teachers.map(t => normalizeString(t));
     }
 }
