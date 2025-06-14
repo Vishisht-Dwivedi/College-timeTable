ceaef4b735f274c40a09f6fc1ff864793242ce02
unknown <vishisht22868@gmail.com>
2025-06-15 03:06:11 +0530

    rewriting all utilities expect for constructor and models

--- server/services/utils/checkCollisions/populateClassroomSchedule.js
+++
@@ -1,8 +0,0 @@
-export function populateClassroomSchedule(schedule, teacherSchedules) {
-    for (const day of schedule) {
-        for (const slot of day.slots) {
-            const scheduleMap = teacherSchedules.get(slot.teacher.code);
-            scheduleMap[day.day].add(slot.slot);
-        }
-    }
-}
